0a306b59a462421746de9e1fcf7a4c6911b1334f
Vivien Millet <vivien.millet@gmail.com>
2021-03-01 17:57:54 +0100

    [premake] cleanup

--- Phantom.lua
+++ Phantom.lua
@@ -51,8 +51,6 @@ solution(PhantomSolutionName)
 	group "Libraries"
 	Vars["Link"] = "Shared"
 	Phantom_add_project("Phantom", Vars)
-
-	group "Extensions"
 	Phantom_add_project("Phantom.Code", Vars)
 	Phantom_add_project("Phantom.JIT", Vars)
 	Phantom_add_project("Phantom.Serialization", Vars)
